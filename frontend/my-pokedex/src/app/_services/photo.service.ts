152b7f792edc587e7641bc684d4926d051e8cddd
Br4yanGC <brayan20160113@gmail.com>
2021-12-11 20:49:16 -0500

    Change camera

---
+++ frontend/my-pokedex/src/app/_services/photo.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  uploadPicture(data) {
+    return this.http.post<any>('http://localhost:3000/figures', data).toPromise();
+  }
+
+  async takePicture(){
+    const capturedPhoto = await Camera.getPhoto({
+      quality: 100,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Base64,
+      allowEditing: true
+    })
+
+    // console.log(capturedPhoto);
+    const response = await this.uploadPicture({picture: capturedPhoto.base64String})
+    // console.log(response)
+    return response;
+  }
+}
